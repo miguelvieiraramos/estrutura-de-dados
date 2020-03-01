85b068f5b777e7dc1ca90e14a83571d492b86aa6
Miguel Vieira Ramos <miguelvieiraramos02@gmail.com>
2020-03-01 17:05:38 -0300

    classe da lista encadeada

---
+++ src/exercicio01/Lista.java
@@ -0,0 +1,26 @@
+package exercicio01;
+
+import java.util.Arrays;
+
+public class Lista {
+    private int[] vetor = new int[10];
+    private int quantidade = 0;
+
+    public String listarVetor() {
+        return Arrays.toString(this.vetor);
+    }
+
+    public void inserirInicio(int numero) {
+        if (quantidade < 10) {
+            for (int i = this.vetor.length - 1; i >= 0; i--) {
+                try {
+                    this.vetor[i] = this.vetor[i - 1];
+                    this.vetor[i - 1] = 0;
+                } catch (IndexOutOfBoundsException e) {
+                    System.out.println(e);
+                }
+            }
+            this.vetor[0] = numero;
+        }
+    }
+}
